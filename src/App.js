49ee540dbe90db21cdbaf29a6f6f8d06a6f6ab56
Ela Korkmaz <elakorkmaz87@gmail.com>
2018-09-22 11:48:33 +0200

    restyled

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '',
-    error: null,
-    isLoaded: false,
-  };
+      this.state = { value: '',
+      error: null,
+      isLoaded: false
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -55,7 +55,7 @@ class App extends Component {
       return (
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-6 ">
+            <div className="col-md-6 ">
               <form>
                 <div className="form-group">
                   <label htmlFor="search">Find a festival:</label>
@@ -64,34 +64,22 @@ class App extends Component {
               </form>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-12">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
               <ul>
-                { festivals.map((festival) =>
+                {festivals.map((festival) =>
                   <div className="card card-outline-danger">
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="card-block">
-                          <h4 className="card-title">
-                            {festival.details.en.title}
-                          </h4>
-                          <p className="card-text">
-                            {festival.details.en.shortdescription}
-                          </p>
-                          <h5 className="card-title">
-                          Date(s):
-                          </h5>
-                          <p className="card-text">
-                            {festival.dates.singles}
-                          </p>
-                        </div>
-                        <div className="card-block">
-                          <a href={festival.urls[0]} className="btn btn-outline-danger"> Link to website </a>
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <img className="card-img-top" src= { festival.media[0].url} alt={festival.title}/>
-                      </div>
+                    <img className="card-img-top img-fluid" src= {festival.media[0].url} alt={festival.title}/>
+                    <div className="card-body">
+                      <h4 className="card-title">{festival.details.en.title}</h4>
+                      <p className="card-text">{festival.details.en.shortdescription}</p>
+                      <h5 className="card-title">Date(s):</h5>
+                      <p className="card-text">
+                        <li key={festival.dates.singles}>
+                          {festival.dates.singlesnk}
+                        </li>
+                      </p>
+                      <h4 className="card-title"><a href={festival.urls[0]} className="btn btn-outline-danger"> Link to website </a></h4>
                     </div>
                   </div>
                 )}
